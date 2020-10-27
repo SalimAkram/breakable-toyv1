b91931edfc6c66fa788f8d66036f1b406d7784b5
Salim Akram <salimakram123@gmail.com>
2020-10-26 23:56:31 -0400

    added roast FactoryBot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -7,4 +7,20 @@ FactoryBot.define do
     password_confirmation { 'password' }
   end
 
+FactoryBot.define do
+  factory :roast do
+    sequence(:name) { |n| "roast #{n}" }
+    sequence(:region) { |n| "region #{n}" }
+    sequence(:notes) { |n| "notes #{n}" }
+    sequence(:process) { |n| "process #{n}" }
+    sequence(:producer) { |n| "producer #{n}" }
+    sequence(:altitude) { |n| "altitude #{n}" }
+    sequence(:url) { |n| "url #{n}" }
+    sequence(:price) { |n| "price #{n}" }
+    sequence(:rating) { |n| "rating #{n}" }
+    fair_trade { true }
+    ethical_business_practices { true }
+  end
+end
+
 end
