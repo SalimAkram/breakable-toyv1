2f39848a285925e4e6826b694c850d6a0c9deb82
Salim Akram <salimakram123@gmail.com>
2020-10-28 21:37:38 -0400

    added user profile option and display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
+
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true 
-
+  mount_uploader :profile_photo, ProfilePhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_005616) do
+ActiveRecord::Schema.define(version: 2020_10_29_000029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_10_28_005616) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "username", null: false
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
