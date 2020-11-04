a1a3b21577b7908dd8b40a9ff66c0eceaeec1b28
Salim Akram <salimakram123@gmail.com>
2020-11-04 00:00:42 -0500

    adding post test for roasts

--- spec/controllers/roasts_controller_spec.rb
+++ spec/controllers/roasts_controller_spec.rb
@@ -1,5 +1,4 @@
 require "rails_helper"
-require "json"
 
 RSpec.describe Api::V1::RoastsController, type: :controller do
 
@@ -26,6 +25,66 @@ RSpec.describe Api::V1::RoastsController, type: :controller do
        expect(returned_json["price"]).to eq roast_1.price
        expect(returned_json["rating"]).to eq roast_1.rating
 
+    end
+  end
+
+  describe "POST#create" do
+    it "adds a new roast" do
+      post_json = {
+        roast: {
+          name: "roast", 
+          brand: "brand",
+          region: "region", 
+          notes: "notes",
+          process: "process", 
+          producer: "producer", 
+          altitude: "altitude", 
+          url: "www.test.com", 
+          price: 25, 
+          rating: 5, 
+          harvest_date: "January 2020", 
+          fair_trade: true, 
+          ethical_business_practices: true, 
+        }
+      }
+      prev_count = Roast.count
+      post(:create, params: post_json, format: :json)
+      expect(Roast.count).to eq(prev_count + 1)
+    end
+
+    it "return the json of the newly posted roast" do
+       post_json = {
+        roast: {
+          name: "heleana", 
+          brand: "map cap",
+          region: "ethiopia", 
+          notes: "berry for days",
+          process: "washed", 
+          producer: "some guy", 
+          altitude: "1900", 
+          url: "www.test.com", 
+          price: 25, 
+          rating: 5, 
+          harvest_date: "January 2020", 
+          fair_trade: true, 
+          ethical_business_practices: true, 
+        }
+      }
+
+      post(:create, params: post_json, format: :json)
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq "application/json"
+      
+      expect(returned_json).to be_kind_of(Hash)
+      expect(returned_json).to_not be_kind_of(Array)
+      expect(returned_json["name"]).to eq "heleana"
+      expect(returned_json["brand"]).to eq "map cap"
+      expect(returned_json["region"]).to eq "ethiopia"
+      expect(returned_json["notes"]).to eq "berry for days"
+      expect(returned_json["process"]).to eq "washed"
+      expect(returned_json["price"]).to eq 25
+      expect(returned_json["rating"]).to eq 5
     end
   end
 end
