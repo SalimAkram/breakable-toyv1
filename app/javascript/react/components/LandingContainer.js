e782284c131832b1152a1aa696c90f1da6ff8fb4
Salim Akram <salimakram123@gmail.com>
2020-11-11 16:40:08 -0500

    added pavement scraper and some styling tweaks

--- app/javascript/react/components/LandingContainer.js
+++ app/javascript/react/components/LandingContainer.js
@@ -59,12 +59,27 @@ navigator.geolocation.getCurrentPosition(successCallback);
         <div className="cell box medium-6" style={{ height: "100%" }}>
             {cafeListArray}
         </div>
-        <div className="cell box grid-y medium-6" style={{ height: "50%" }}>
-          <div className="test cell medium-6 top">
-            <h1>Search Bar</h1>
+        <div className="cell box grid-y medium-2">
+          <div className="card-user-container" >
+            <div class="card-user-avatar">
+              <img src="https://placehold.it/350x350" alt="" class="user-image"></img>
+            </div>
+            <div class="card-user-social">
+              <ul class="menu">
+                <li class="fa fa-twitter"></li>
+                <li class="fa fa-dribbble"></li>
+                <li class="fa fa-instagram"></li>
+                <li class="fa fa-github"></li>
+              </ul>
+            </div>
+            <div class="card-user-bio">
+              <h4>User Name</h4>
+              <p>UX/UI ,Front-end developer, Foundation interested. </p>
+              <span class="location"><span class="location-icon fa fa-map-marker"></span><span class="location-text">Makkah Al-Mukaramah</span></span>
+            </div>
+            <div class="card-user-button">
+            <a href="#" class="button">FOLLOW</a>
           </div>
-          <div className="test cell medium-6">
-            <h1>Black Owned</h1>
           </div>
         </div>
       </div>
