47cbeed0a7604dcde07460e455eaa8bb0767c5be
Salim Akram <salimakram123@gmail.com>
2020-12-11 13:56:05 -0500

    added react and rails error handling  and form field reset

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -4,10 +4,17 @@ class Api::V1::SearchesController < ApplicationController
       render json: "cant be blank"
     else
       coords = Coords.coords(params[:location])
-      lat = coords[0]
-      long = coords[1]
-      shops = Cafe.cafe_search(lat, long)
+      if coords[0] == "hmm....that search was weird try it one more time"
+        error = coords[0]
+      else
+        lat = coords[0]
+        long = coords[1]
+        shops = Cafe.cafe_search(lat, long)
+          if shops.count == 0
+            error = "hmm....that search was weird try it one more time"
+          end
+      end
     end 
-    render json: shops
+    render json: {shops: shops, error: error }
   end
 end 
